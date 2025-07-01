e685b90412218d019904ca83bc90ca5633f36c47
Komal Shinde <shindekomalkr@gmail.com>
2025-07-01 12:46:02 +0530

    Updated gallery filter and services

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import './App.css';
 import Home from './components/Home';
 import About from './components/About';
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { SpeedInsights } from "@vercel/speed-insights/react"
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
